4265f9dc77b68703763c2c1371cfac9a16b5c800
Artyom Gavrilin <artyomgavrilin@MacBook-Pro-Artyom.local>
2021-09-02 08:30:21 +0300

    Init commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+// registers aliases, DON'T REMOVE THIS LINE!
+import 'module-alias/register';
+
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, ValidationError } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import ValidationExceptions from './exceptions/validation.exceptions';
+
+import AppModule from './routes/app/app.module';
+
+import AllExceptionsFilter from './filters/all-exceptions.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors: ValidationError[]) => new ValidationExceptions(errors),
+  }));
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  const port = process.env.SERVER_PORT || 3000;
+
+  const options = new DocumentBuilder()
+    .setTitle('Api v1')
+    .setDescription('The boilerplate API for nestjs devs')
+    .setVersion('1.0')
+    .addBearerAuth({ in: 'header', type: 'http' })
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port, async () => {
+    console.log(`The server is running on ${port} port: http://localhost:${port}/api`);
+  });
+}
+bootstrap();
